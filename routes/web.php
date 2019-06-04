05ba727b111a2e546b27e2518d6952f0a12f9db3
Sergey <kompwen7@gmail.com>
2019-06-04 21:49:40 +0300

    +Authentication

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
 
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
    Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-Route::resource('rest', 'RestTestController')->names('restTest');
+// Админка для блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'    => 'admin/blog',
+];
+Route::group($groupData, function () {
+    $methods = ['index', 'edit', 'update', 'create', 'store',];
+    Route::resource('categoties', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+
+
+
+
+
+
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
